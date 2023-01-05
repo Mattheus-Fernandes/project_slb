54891c892aa326121cd355deb0c7f14ec89eeb7b
Mattheus Fernandes <82783833+Mattheus-Fernandes@users.noreply.github.com>
2023-01-05 10:45:27 -0300

    Update main.js

--- consumo_basket_api/script/main.js
+++ consumo_basket_api/script/main.js
@@ -2,7 +2,7 @@ const allBasketsAvailable = document.getElementById('allBasketsAvailable')
 const allBasketsSegregated = document.getElementById('allBasketSegregated')
 
 
-axios.get('https://front-project-slb.onrender.com/basket')
+axios.get('https://project-slb.onrender.com/basket')
     .then(baskets => {
         
         const responseBaskets = baskets.data.basketAvailable
